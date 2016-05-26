58313585ebcedf6613d33626d428f92a4ae5e846
Scott Burton <scotthburton@gmail.com>
2016-05-26 11:21:22 -0600

    Added line to force php to throw exceptions on pdo errors

--- 0526_movies/dbConnect.php
+++ 0526_movies/dbConnect.php
@@ -8,6 +8,10 @@ function connectToDb()
 		$password = "nerdface";
 
 		$db = new PDO('mysql:host=127.0.0.1;dbname=movies', $user, $password);
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
 	}
 	catch (PDOException $ex) 
 	{
